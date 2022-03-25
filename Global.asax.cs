6c9053665b11a02598b41d45e5b3c45cf1938d57
Nabeel Patel <P2645080@my365.dmu.ac.uk>
2022-03-25 04:40:35 +0000

    test

--- Global.asax.cs
+++ Global.asax.cs
@@ -6,8 +6,10 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+test
 namespace Error_Collage
 {
+   
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
