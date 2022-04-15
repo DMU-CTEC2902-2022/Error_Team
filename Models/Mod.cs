81f654f329c85f0d18d2761d3d6ad5034d6d2fd5
Raj Patel <P2626408@my365.dmu.ac.uk>
2022-04-15 10:58:40 +0100

    New Layout Color

---
+++ Models/Mod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Error_Collage.Models
+{
+    public class Mod
+    {
+        public virtual int ModuleId { get; set; }
+
+        public virtual int CourseId { get; set; }
+
+        public virtual string ModuleCode { get; set; }
+
+        public virtual string ModuleTitle { get; set; }
+
+        public virtual string Faculty { get; set; }
+
+        public virtual string Content { get; set; }
+
+        public virtual string Description { get; set; }
+    }
+}
