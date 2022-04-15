6052db0fa07f48b71cc32d25e6aae347cef79dfc
Raj Patel <P2626408@my365.dmu.ac.uk>
2022-04-15 13:02:32 +0100

    Solve F

--- Data/MolduleContext.cs
+++ Data/MolduleContext.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace Error_Collage.Data
 {
-    public class ModuleContext : DbContext
+    public class MolduleContext : DbContext
     {
         // You can add custom code to this file. Changes will not be overwritten.
         // 
@@ -15,10 +15,10 @@ namespace Error_Collage.Data
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
     
-        public ModuleContext() : base("name=ModuleContext")
+        public MolduleContext() : base("name=MolduleContext")
         {
         }
 
-        public DbSet<Error_Collage.Models.Module> Modules { get; set; }
+        public System.Data.Entity.DbSet<Error_Collage.Models.Module> Modules { get; set; }
     }
 }
--- Models/Module.cs
+++ Models/Module.cs
@@ -7,18 +7,42 @@ namespace Error_Collage.Models
 {
     public class Module
     {
-        public virtual int ModuleId { get; set; }
+        //module model for Computer Science course
+        public string ModuleCode { get; set; }
+        public string ModuleTitle { get; set; }
+        public string ModuleDescription { get; set; }
+        public string ModuleContents { get; set; }
+        public string StaffMembers { get; set; }
 
-        public virtual int CourseId { get; set; }
-
-        public virtual string ModuleCode { get; set; }
-
-        public virtual string ModuleTitle { get; set; }
-
-        public virtual string Faculty { get; set; }
-
-        public virtual string Content { get; set; }
-
-        public virtual string Description { get; set; }
+        //module model for Computer Science course
+        public string ModuleCode1 { get; set; }
+        public string ModuleTitle1 { get; set; }
+        public string ModuleDescription1 { get; set; }
+        public string ModuleContents1 { get; set; }
+        public string StaffMembers1 { get; set; }
+        //module model for Computer Science course
+        public string ModuleCode2 { get; set; }
+        public string ModuleTitle2 { get; set; }
+        public string ModuleDescription2 { get; set; }
+        public string ModuleContents2 { get; set; }
+        public string StaffMembers2 { get; set; }
+        //module model for Computer Science course
+        public string ModuleCode3 { get; set; }
+        public string ModuleTitle3 { get; set; }
+        public string ModuleDescription3 { get; set; }
+        public string ModuleContents3 { get; set; }
+        public string StaffMembers3 { get; set; }
+        //module model for Computer Science course
+        public string ModuleCode4 { get; set; }
+        public string ModuleTitle4 { get; set; }
+        public string ModuleDescription4 { get; set; }
+        public string ModuleContents4 { get; set; }
+        public string StaffMembers4 { get; set; }
+        //module model for Computer Science course
+        public string ModuleCode5 { get; set; }
+        public string ModuleTitle5 { get; set; }
+        public string ModuleDescription5 { get; set; }
+        public string ModuleContents5 { get; set; }
+        public string StaffMembers5 { get; set; }
     }
 }
