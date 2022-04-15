58cf6d5279ec328c57cdd956039afa8ee46b2873
Raj Patel <P2626408@my365.dmu.ac.uk>
2022-04-15 01:16:34 +0100

    NEW F

---
+++ Data/ModuleContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Error_Collage.Data
+{
+    public class ModuleContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ModuleContext() : base("name=ModuleContext")
+        {
+        }
+
+        public DbSet<Error_Collage.Models.Module> Modules { get; set; }
+    }
+}
---
+++ Models/Faculty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Error_Collage.Models
+{
+    public class Faculty
+    {
+        public virtual int FacultyId { get; set; }
+
+        public virtual string FacultyName { get; set; }
+
+        public virtual string TeachingFeild {get; set; }
+
+        public virtual string Qualification { get; set; }
+
+        public virtual string Deccription { get; set; }
+
+        public virtual string Courseid { get; set; }
+
+       
+
+    }
+}
